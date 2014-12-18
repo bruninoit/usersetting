b11ea95f12537ce12e24dab7d3d0b3e2417d2bfb
bruninoit <brunino98@hotmail.it>
2014-12-18 11:40:43 +0100

    Create common.php

---
+++ language/it/common.php
@@ -0,0 +1,21 @@
+<?php
+/**
+*
+* @package StaffIt - Top Ten Topics
+* @copyright (c) 2014 brunino
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+if (!defined('IN_PHPBB'))
+{
+exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+$lang = array();
+}
+$lang = array_merge($lang, array(
+'ACP_US_TITLE'	=> 'Generali',
+'ACP_US_CATEGORY'	=> 'Users Setting',
+
+));
